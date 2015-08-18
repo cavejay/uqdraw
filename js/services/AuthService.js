c2e7d586879595ff8cb9f0cdec5c87f2d96aa01c
Michael B <michael@sablecliff.com>
2015-08-18 11:01:53 +1000

    This is the most hacky thing I've ever done and will need to be fixed so badly. But logins are now working :D (but only logins, sign-ups are not yet implemented)

--- js/services/AuthService.js
+++ js/services/AuthService.js
@@ -3,9 +3,14 @@ let firebaseRoot = config.firebase.base;
 let Firebase = require('firebase');
 import LoginActions from '../actions/LoginActions.js';
 
+let EventEmitter = require('events').EventEmitter;
+
 let AuthService = {
+  emitter: new EventEmitter()
+}
+
   // Checks if user is in the system
-  login: function(username, password) {
+AuthService.login = function(username, password, callback, router) {
 
     // Access the database's Users list
     var usersRef = new Firebase(firebaseRoot+"/Users");
@@ -16,27 +21,30 @@ let AuthService = {
       // Check if we actually got something
       if (snapshot.val() == null) {
         console.log("We didn't get anything :'( - " + snapshot.val());
-        return false;
+        return 0;
 
       } else {
-        usersRef.child(username).on("value", function(snapshot) {
+        usersRef.child(username).once("value", function(snapshot) {
 
           // We should have got data and we can compare it to the pword
           var returnPass = snapshot.val();
           if (returnPass == password){
             console.log("We matched the password!\n "+returnPass+"=="+password);
-            return true;
+            callback(username, router);
+            return 0;
           }
         });
       }
     });
+
+    return 0;
   }
 
   // creates an entry for username w/ a password of password.
   // Returns 0 if the user already exists
-  create: function(username, pasword) {
+AuthService.create = function(username, pasword) {
     return 0;
   }
-}
+
 
 export default AuthService;
