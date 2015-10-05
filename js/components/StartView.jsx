dd8fea9f2f4f22bc4993ec183502a907289ca0d0
Michael B <michael@sablecliff.com>
2015-10-05 23:29:13 +1000

    I'm a scrub and didn't declare a var

--- js/components/StartView.jsx
+++ js/components/StartView.jsx
@@ -4,7 +4,7 @@ import AuthActions from '../actions/AuthActions.js';
 import AuthStore from '../stores/AuthStore.js';
 require('../../css/components/StartView.scss');
 
-versionNumber = 'v0.2.2'
+var versionNumber = 'v0.2.2'
 
 class StartView extends React.Component {
   constructor(props) {
